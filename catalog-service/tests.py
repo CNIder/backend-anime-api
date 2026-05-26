a35c77360795adb3f3f810c420891ed2a2822ba1
CNIder <38539977+CNIder@users.noreply.github.com>
2026-05-26 15:06:07 +0100

    Update tests.py

--- catalog-service/tests.py
+++ catalog-service/tests.py
@@ -7,5 +7,5 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 204
     assert response.json() == {"status": "ok"}
